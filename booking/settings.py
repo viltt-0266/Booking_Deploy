19495844bd8699864851ea666024d2fb35d330a6
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-08-03 19:50:10 +0700

    sds

--- booking/settings.py
+++ booking/settings.py
@@ -22,8 +22,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+DATABASE_URL = 'mysql://root:123456789@localhost:3306/my_booking'
 
+ALLOWED_HOSTS = ["web-vivivideploy.up.railway.app",'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ["https://web-vivivideploy.up.railway.app/",'https://*.127.0.0.1']
+
+CORS_ALLOWED_ORIGINS = [
+    "https://web-vivivideploy.up.railway.app/",]
 
 # Application definition
 
